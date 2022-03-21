749285bedc2b1a663af54aa5e477161ef1bfbf42
SimDaeSung <86938974+ShimDaeSung1@users.noreply.github.com>
2022-03-21 20:25:06 +0900

    A-->B

--- greedy/baekjoon16953.py
+++ greedy/baekjoon16953.py
@@ -10,17 +10,25 @@ value = B
 
 while value>A:
     if value % 10 == 1:
-        value = B//10
+        value = value//10
         cnt+=1
+        if(value<A):
+            print('-1')
+            break
     # 2로 나누어 떨어지지도 않고 끝이 1도 아니라면 -1출력
     elif value % 2 == 1 and value % 10 != 1:
         print('-1')
+        break
     else : 
         value = value//2
         cnt+=1
+        if(value<A):
+            print('-1')
+            break
     if value == A :
+        print(cnt)
         break 
-print(cnt)
+
 
 
 
