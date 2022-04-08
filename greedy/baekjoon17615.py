f0d5ea09a1a8868cc625a0f71cee80fe2d5927fc
SimDaeSung1 <86938974+ShimDaeSung1@users.noreply.github.com>
2022-04-08 14:48:37 +0900

    Update baekjoon17615.py

--- greedy/baekjoon17615.py
+++ greedy/baekjoon17615.py
@@ -2,14 +2,24 @@ import sys
 input = sys.stdin.readline
 
 N = int(input())
-balls = input()
+balls = input().strip()
 
 count_list = []
 
-# 맨 왼쪽에 있는 R 하나 제거 후 모든 R을 왼쪽으로 옮기는 케이스
-# parse_ball = balls.lstrip('R')
-# count_list.append(parse_ball.count('R'))
+# 맨 왼쪽에 있는 R 뭉텅이 제거 후 모든 R을 왼쪽으로 옮기는 케이스
+parse_ball = balls.lstrip('R')
+count_list.append(parse_ball.count('R'))
 
-# 맨 오른쪽에 있는 R 하나 제거 후 모든 R을 오른쪽으로 옮기는 케이스
+# 맨 오른쪽에 있는 R 뭉텅이 제거 후 모든 R을 오른쪽으로 옮기는 케이스
 parse_ball = balls.rstrip('R')
-print(parse_ball)
+count_list.append(parse_ball.count('R'))
+
+# 맨 왼쪽에 있는 B 뭉텅이 제거 후 모든 B를 왼쪽으로 옮기는 경우
+parse_ball = balls.lstrip('B')
+count_list.append(parse_ball.count('B'))
+
+# 맨 오른쪽에 있는 B 뭉텅이 제거 후 모든 B를 오른쪽으로 옮기는 경우
+parse_ball = balls.rstrip('B')
+count_list.append(parse_ball.count('B'))
+
+print(min(count_list))
