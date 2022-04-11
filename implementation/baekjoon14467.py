80c451e4742405186506aa909fef22869eafbf7d
SimDaeSung1 <86938974+ShimDaeSung1@users.noreply.github.com>
2022-04-11 15:55:02 +0900

    Create baekjoon14467.py

---
+++ implementation/baekjoon14467.py
@@ -0,0 +1,16 @@
+import sys
+input =sys.stdin.readline
+
+N = int(input())
+cow = [list(map(int, input().split()))for _ in range(N)]
+count = 0
+cow_po = [-1]*11
+
+for num, position in cow :
+    if cow_po[num] == -1 :
+        cow_po[num] = position
+    else:
+        if cow_po[num] != position:
+            count+=1
+            cow_po[num] = position
+print(count)
