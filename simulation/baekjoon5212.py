d6b8301412ae3b39bbb17d618e3548fd35eb48be
SimDaeSung1 <86938974+ShimDaeSung1@users.noreply.github.com>
2022-07-04 22:15:26 +0900

    Update baekjoon5212.py

--- simulation/baekjoon5212.py
+++ simulation/baekjoon5212.py
@@ -2,7 +2,7 @@ import copy
 import sys
 input = lambda: sys.stdin.readline().strip()
 
-# 50년 뒤 사라지는 섬이면 False, 살아있는 섬이면 True리턴
+# 50년 뒤 사라지는 섬이면 False, 살아있는 섬이면 True리턴  
 def survive_land(x, y, data_map):
     if data_map[x+1][y] + data_map[x-1][y] + data_map[x][y+1] + data_map[x][y-1] >= 3:
         return False
