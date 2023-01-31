38a8e9fc7f4896a5abb8a9fdf03ef7151d195562
SimDaeSung <86938974+ShimDaeSung1@users.noreply.github.com>
2023-01-31 19:07:24 +0900

    backtracking

---
+++ backtracking/baekjoon15657.py
@@ -0,0 +1,19 @@
+import sys
+input = sys.stdin.readline
+sys.setrecursionlimit(10**6)
+
+n, m = map(int, input().split())
+arr = list(map(int, input().split()))
+arr.sort()
+s = []
+
+def backtracking(start):
+    if len(s) == m:
+        print(' '.join(map(str, s)))
+        return
+    for i in range(start, n):
+        s.append(arr[i])
+        backtracking(i)
+        s.pop()
+backtracking(0)
+
