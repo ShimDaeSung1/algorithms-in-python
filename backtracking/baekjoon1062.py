dc87eec135052d278f849a49f633a30daba3388a
SimDaeSung1 <86938974+ShimDaeSung1@users.noreply.github.com>
2023-04-26 23:36:43 +0900

    Update baekjoon1062.py

--- backtracking/baekjoon1062.py
+++ backtracking/baekjoon1062.py
@@ -1,61 +1,47 @@
-# // 남극에 사는 김지민 선생님은 학생들이 되도록이면 많은 단어를 읽을 수 있도록 하려고 한다. 그러나 지구온난화로 인해 얼음이 녹아서 
-# // 곧 학교가 무너지기 때문에, 김지민은 K개의 글자를 가르칠 시간 밖에 없다. 김지민이 가르치고 난 후에는, 학생들은 그 K개의 글자로만 이루어진 단어만을 읽을 수 있다.
-# //  김지민은 어떤 K개의 글자를 가르쳐야 학생들이 읽을 수 있는 단어의 개수가 최대가 되는지 고민에 빠졌다.
-
-# // 남극언어의 모든 단어는 "anta"로 시작되고, "tica"로 끝난다. 남극언어에 단어는 N개 밖에 없다고 가정한다. 학생들이 읽을 수 있는 단어의 최댓값을 구하는 프로그램을 작성하시오.
-
-# // 입력
-# // 첫째 줄에 단어의 개수 N과 K가 주어진다. N은 50보다 작거나 같은 자연수이고, K는 26보다 작거나 같은 자연수 또는 0이다. 둘째 줄부터 N개의 줄에 남극 언어의 단어가 주어진다. 
-# // 단어는 영어 소문자로만 이루어져 있고, 길이가 8보다 크거나 같고, 15보다 작거나 같다. 모든 단어는 중복되지 않는다.
-
-# // 출력
-# // 첫째 줄에 김지민이 K개의 글자를 가르칠 때, 학생들이 읽을 수 있는 단어 개수의 최댓값을 출력한다.
-
-# 글자 아는지 체크
-def check(knowList, mustKnow):
-    mustKnow = deepcopy(mustKnow)
-    visited = [False for _ in range(len(mustKnow))]
-    for k in knowList:
-        if k in mustKnow:
-            mustKnow.remove(k)
-    # 모르는 단어 있음
-    if len(mustKnow) > 0 :
-        return False
-    else:
-        return True
-from copy import deepcopy
 import sys
-input = sys.stdin.readline
 
-basic = 97
-mustKnow = ['a','n','t', 'i','c']
-n,k = map(int, input().split())
-words = list(input().strip() for _ in range(n))
+n, k = map(int, input().split())
 
-visited = [False for _ in range(26)] # 0 ~ 25까지, 97 ~ 97+25, a~z
-ans = 0
-know = [] # 아는 글자, len이 K를 넘을수없음
-def backtracking(depth):
-    if depth == k:
-        # 필수글자를 안다면
-        if check(know, mustKnow):
-            answer = 0
-            for word in words:
-                if check(know, word):
-                    answer += 1
-            ans = max(ans, answer)
+# k 가 5보다 작으면 어떤 언어도 배울 수 없음
+if k < 5:
+    print(0)
+    exit()
+# k 가 26이면 모든 언어를 배울 수 있음
+elif k == 26:
+    print(n)
+    exit()
+
+answer = 0
+words = [set(sys.stdin.readline().rstrip()) for _ in range(n)]
+learn = [0] * 26
+
+# 적어도 언어 하나는 배우기위해 a,c,i,n,t 는 무조건 배워야함
+for c in ('a', 'c', 'i', 'n', 't'):
+    learn[ord(c) - ord('a')] = 1
+
+
+def dfs(idx, cnt):
+    global answer
+
+    if cnt == k - 5:
+        readcnt = 0
+        for word in words:
+            check = True
+            for w in word:
+                if not learn[ord(w) - ord('a')]:
+                    check = False
+                    break
+            if check:
+                readcnt += 1
+        answer = max(answer, readcnt)
         return
-        
-    for i in range(26):
-        if visited[i] == False:
-            know.append(chr(basic + i))
-            visited[i] = True
-            backtracking(depth+1)
-            visited[i] = False
-            know.pop()
-backtracking(0)
+
+    for i in range(idx, 26):
+        if not learn[i]:
+            learn[i] = True
+            dfs(i, cnt + 1)
+            learn[i] = False
 
 
-
-
-
+dfs(0, 0)
+print(answer)
